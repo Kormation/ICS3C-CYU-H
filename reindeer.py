de5a8caf8383cdea999c4f8646c56df36f6920ab
Kormation <145355933+Kormation@users.noreply.github.com>
2023-12-13 08:53:36 -0500

    started program
    n/a

--- reindeer.py
+++ reindeer.py
@@ -1,3 +1,11 @@
-# Programmer: 
-# Description: 
+# Programmer : Alexander Walker
+# Description : N/A
+
+santas_reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donder", "Blitzen"]
+
+print(f'More rapid than eagles his coursers they came, And he whistled, and shouted, and called')
+print(f'them by name; "Now, {santas_reindeer}!, now {santas_reindeer}! now {santas_reindeer} and')
+print(f'{santas_reindeer}! On, {santas_reindeer}! on, {santas_reindeer}! on, {santas_reindeer}')
+print(f'and {santas_reindeer}! To the top of the porch! to the top of the wall! Now dash away!')
+print(f'dash away! dash away all!')
 
